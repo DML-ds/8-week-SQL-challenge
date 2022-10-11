4ae8471184710f563b1c7f8d2be3fa278e474a95
Damilare Orogun <82219107+DML-ds@users.noreply.github.com>
2022-10-11 04:10:30 +0100

    Update Solutions.sql

--- Case #2 - Pizza Runner/Solutions.sql
+++ Case #2 - Pizza Runner/Solutions.sql
@@ -54,5 +54,92 @@ ORDER BY
 	c.customer_id;
 	
 	
+--6. What was the maximum number of pizzas delivered in a single order?
+WITH pizza_count_cte as(
+SELECT
+	c.order_id,
+	count(pizza_id) as order_per_customer
+FROM
+	customers_orders_temp as c JOIN runner_orders_temp as r
+	ON c.order_id = r.order_id
+WHERE r.distance != 0
+GROUP BY
+	c.order_id
+ORDER BY
+	c.order_id
+)
+SELECT
+	MAX(order_per_customer) as max_pizza_delivered
+FROM
+	pizza_count_cte;
 
 
+
+
+--6. What was the maximum number of pizzas delivered in a single order?
+WITH pizza_count_cte as(
+SELECT
+	c.order_id,
+	COUNT(r.order_id) as pizza_count,
+	RANK() OVER(ORDER BY COUNT(r.order_id)DESC)
+FROM
+	customers_orders_temp as c JOIN runner_orders_temp as r
+	ON c.order_id = r.order_id
+WHERE
+	distance != 0
+GROUP BY
+	c.order_id
+)
+SELECT
+	order_id,
+	pizza_count
+FROM
+	pizza_count_cte
+WHERE
+	rank = 1
+
+
+--7.For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+CREATE view delivered_order AS
+SELECT * FROM customers_orders_temp
+JOIN runner_orders_temp
+using (order_id)
+WHERE distance IS NOT null;
+
+SELECT
+	customer_id,
+	COUNT(
+		CASE
+			WHEN exclusions <> null or extras <> null THEN 1
+		END) AS pizza_change,
+	COUNT(
+		CASE
+			WHEN exclusions is null or extras is null THEN 1
+		END) AS no_pizza_change
+FROM
+	delivered_order
+GROUP BY
+	customer_id
+ORDER BY
+	customer_id;
+
+
+
+
+
+
+
+--9. How many pizzas were delivered that had both exclusions and extras?
+SELECT 
+	hour(order_time) AS 'hour',
+    	count(order_id) AS 'num_pizza_ordered',
+    	round(100*count(order_id) /sum(count(order_id)) over(), 2) AS 'volume_ordered'
+FROM 
+	pizza_runner.customers_orders_temp
+GROUP BY 
+	1
+ORDER BY
+	1;
+	
+	
+--9. 
